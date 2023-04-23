3313d917c96ec128c51dc1e58b087b1bc088ca7f
Meezaan <meezaandavids365@gmail.com>
2023-04-23 19:12:51 +0200

    update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/news', (req, res) => {
   const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${NEWS_API_KEY}`;
   axios.get(url)
     .then(response => {
+      res.header('Access-Control-Allow-Origin', '*');
       res.json(response.data);
     })
     .catch(error => {
@@ -16,13 +17,6 @@ app.get('/news', (req, res) => {
     });
 });
 
-app.use((req, res, next)=> {
-  req.header('Access-Control-Allow-Origin', 'http://localhost:8080/')
-  res.header("Access-Control-Allow-Credentials", "true")
-  res.header("Access-Control-Allow-Methods", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next();
-});
 
 app.listen(port, () => {
   console.log(`BNRY backend is running on port ${port}`);
